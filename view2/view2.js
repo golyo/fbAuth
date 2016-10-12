853d37a7aeb2a499cf89af456e92a411498c3d56
Szendrei Tamas <golyo@sonrisa.hu>
2016-10-12 21:23:02 +0200

    add auth to view2.js

--- view2/view2.js
+++ view2/view2.js
@@ -1,14 +1,16 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute'])
+angular.module('myApp.view2', ['ngRoute', 'fbAuth'])
 
-.config(['$routeProvider', function($routeProvider) {
+.config(['$routeProvider', '$authProvider', function($routeProvider, $authProvider) {
   $routeProvider.when('/view2', {
     templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
+    controller: 'View2Ctrl',
+    resolve: $authProvider.resolve
   });
 }])
 
-.controller('View2Ctrl', function($rootScope, $location) {
+.controller('View2Ctrl', function($rootScope, $location, user) {
     console.log('View2Ctrl');
+    console.log(user);
 });
